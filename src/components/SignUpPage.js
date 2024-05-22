b46bfa97c796416505105160ce5a82ceabc239db
daviddexteel <dsilvestri@dexteel.com>
2024-05-22 00:20:17 -0300

    SignUp needs also externalId and externalType

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -9,14 +9,16 @@ export const SignUpPage = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [email, setEmail] = useState('')
+    const [email, setEmail] = useState('');
+    const [externalId, setExternalId] = useState('');
+    const [externalType, setExternalType] = useState('');
     const dispatch = useDispatch();
 
   return (
     <div style={{width:'30rem',margin:'auto',paddingTop:'8px'}}>
         <Form onSubmit={event=> {
             event.preventDefault();
-            SignUp(dispatch,{username,password,email});
+            SignUp(dispatch,{username,password,email,externalId,externalType});
         }}>
             <h4 style={{textAlign:'center'}}>Sign Up</h4>
             <InputGroup className='mb-3'>
